69e1520a8a75842708382407c1552f6d3b261f5a
pranav <prnvthir@gmail.com>
2021-04-06 12:55:06 -0400

    add calcAge function solution

--- edabit/practiceKatasJS.js
+++ edabit/practiceKatasJS.js
@@ -1,1 +1,13 @@
-//
+//Create a function that takes the age and return the age in days.
+
+// function calcAge () {}
+
+// const calcAge = (age) => {
+//   return age * 365;
+// };
+
+// console.log("calcAge(65)", calcAge(65));
+// console.log("---");
+// console.log("calcAge(0)", calcAge(0));
+// console.log("---");
+// console.log("calcAge(20)", calcAge(20));
