991ab00e954850bee8cd2d079d32b6290b9eae16
pranav <prnvthir@gmail.com>
2021-04-07 17:47:28 -0400

    add somethingString solution

--- edabit/practiceKatasJS.js
+++ edabit/practiceKatasJS.js
@@ -11,3 +11,19 @@
 // console.log("calcAge(0)", calcAge(0));
 // console.log("---");
 // console.log("calcAge(20)", calcAge(20));
+
+
+//Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+const somethingString = (string) => {
+
+  return `something ${string}`
+
+}
+
+
+console.log(somethingString("is better than nothing")) 
+console.log('1`````````````````````````````````````')
+console.log(somethingString("Bob Jane"))
+console.log('1`````````````````````````````````````')
+console.log(somethingString("something"))
