3bb4f70da6badd42f4ee4cd817bbec063c8c4923
pranav <prnvthir@gmail.com>
2021-04-07 18:15:30 -0400

    add soltuion for shiftToLeft function (shiftToLeft(10,3) = 10 * 2^3 = 10 * 8 = 80)

--- edabit/practiceKatasJS.js
+++ edabit/practiceKatasJS.js
@@ -44,16 +44,41 @@
 
 // Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
 
-const addUp = (num) => {
-  let sumToReturn = 0;
-  for (let i = 0; i <= num; i++) {
-    sumToReturn += i;
-  }
-  return sumToReturn;
+// const addUp = (num) => {
+//   let sumToReturn = 0;
+//   for (let i = 0; i <= num; i++) {
+//     sumToReturn += i;
+//   }
+//   return sumToReturn;
+// };
+
+// console.log(addUp(4));
+// console.log("1`````````````````````````````````````");
+// console.log(addUp(13));
+// console.log("1`````````````````````````````````````");
+// console.log(addUp(600));
+
+// The left shift operation is similar to multiplication by powers of two.
+// Sample calculation using the left shift operator (<<):
+// 10 << 3 = 10 * 2^3 = 10 * 8 = 80
+// -32 << 2 = -32 * 2^2 = -32 * 4 = -128
+// 5 << 2 = 5 * 2^2 = 5 * 4 = 20
+// Write a function that mimics (without the use of <<) the left shift operator and returns the result from the two given integers.
+
+const shiftToLeft = (x, y) => {
+  let returnVal = 0;
+
+  let numToBeSquaredSquared = 2 ** y;
+
+  returnVal = x * numToBeSquaredSquared;
+
+  return returnVal; 
 };
 
-console.log(addUp(4));
-console.log("1`````````````````````````````````````");
-console.log(addUp(13));
-console.log("1`````````````````````````````````````");
-console.log(addUp(600));
+
+console.log(shiftToLeft(5, 2))    
+console.log(shiftToLeft(10, 3)) 
+console.log(shiftToLeft(-32, 2))
+console.log(shiftToLeft(-6, 5))
+console.log(shiftToLeft(12, 4))
+console.log(shiftToLeft(46, 6))
