6c551665be2f49f8d5048747de0ab7542ff9f01d
pranav <prnvthir@gmail.com>
2021-04-07 19:17:39 -0400

    add solution for canNest checks criteria before inserting one array into another

--- edabit/practiceKatasJS.js
+++ edabit/practiceKatasJS.js
@@ -60,25 +60,77 @@
 
 // The left shift operation is similar to multiplication by powers of two.
 // Sample calculation using the left shift operator (<<):
-// 10 << 3 = 10 * 2^3 = 10 * 8 = 80
+// shiftToLeft(10,3) = 10 * 2^3 = 10 * 8 = 80
 // -32 << 2 = -32 * 2^2 = -32 * 4 = -128
 // 5 << 2 = 5 * 2^2 = 5 * 4 = 20
 // Write a function that mimics (without the use of <<) the left shift operator and returns the result from the two given integers.
 
-const shiftToLeft = (x, y) => {
-  let returnVal = 0;
+// const shiftToLeft = (x, y) => {
+//   let returnVal = 0;
 
-  let numToBeSquaredSquared = 2 ** y;
+//   let numToBeSquaredSquared = 2 ** y;
 
-  returnVal = x * numToBeSquaredSquared;
+//   returnVal = x * numToBeSquaredSquared;
 
-  return returnVal; 
-};
+//   return returnVal;
+// };
 
+// console.log(shiftToLeft(5, 2))
+// console.log(shiftToLeft(10, 3))
+// console.log(shiftToLeft(-32, 2))
+// console.log(shiftToLeft(-6, 5))
+// console.log(shiftToLeft(12, 4))
+// console.log(shiftToLeft(46, 6))
 
-console.log(shiftToLeft(5, 2))    
-console.log(shiftToLeft(10, 3)) 
-console.log(shiftToLeft(-32, 2))
-console.log(shiftToLeft(-6, 5))
-console.log(shiftToLeft(12, 4))
-console.log(shiftToLeft(46, 6))
+// Create a function that returns true if the first array can be nested inside the second.
+// arr1 can be nested inside arr2 if:
+// arr1's min is greater than arr2's min.
+// arr1's max is less than arr2's max.
+
+// function canNest(arr1, arr2) {
+//   //check: arr1's min is greater than arr2's min && arr1's max is less than arr2's max.
+
+//   //method; 1 not using any built in methods
+
+//   //first need to get min value in arr 1 and arr 2
+//   let arr1min = Infinity;
+//   let arr2min = Infinity;
+
+//   let arr1max = -Infinity;
+//   let arr2max = -Infinity;
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] < arr1min) {
+//       arr1min = arr1[i];
+//     }
+
+//     if (arr1[i] > arr1max) {
+//       arr1max = arr1[i];
+//     }
+//   }
+
+//   for (let i = 0; i < arr2.length; i++) {
+//     if (arr2[i] < arr2min) {
+//       arr2min = arr2[i];
+//     }
+
+//     if (arr2[i] > arr2max) {
+//       arr2max = arr2[i];
+//     }
+//   }
+//   //console.log(`${arr1min} > ${arr2min} && ${arr1max} < ${arr2max}`)
+//   //check: arr1's min is greater than arr2's min && arr1's max is less than arr2's max.
+//   if (arr1min > arr2min && arr1max < arr2max) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(canNest([1, 2, 3, 4], [0, 6]));
+// console.log("------------------------------");
+// console.log(canNest([3, 1], [4, 0]));
+// console.log("------------------------------");
+// console.log(canNest([9, 9, 8], [8, 9]));
+// console.log("------------------------------");
+// console.log(canNest([1, 2, 3, 4], [2, 3]));
