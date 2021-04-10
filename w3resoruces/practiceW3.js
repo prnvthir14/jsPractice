b9403ad241e9d1c29182de18cc6540693b40ef7a
pranav <prnvthir@gmail.com>
2021-04-09 22:59:18 -0400

    add solution for checkForPalindrom: function that checks if string is a palindrome

--- w3resoruces/practiceW3.js
+++ w3resoruces/practiceW3.js
@@ -2,21 +2,49 @@
 // Example x = 32243;
 // Expected Output : 34223
 
-const reverseInt = (number) => {
-  //take x, an int and return inverse of x
-  let numberReverseStr = '';
-  let numberToString = number.toString();
-  let counter = (numberToString.length - 1);
+// const reverseInt = (number) => {
+//   //take x, an int and return inverse of x
+//   let numberReverseStr = '';
+//   let numberToString = number.toString();
+//   let counter = (numberToString.length - 1);
 
-  //aprch 1
-  //loop through number from end to begining and add each digit to numberReverse as we go
-  for (let i = counter; i >= 0; i--) {
-    // console.log(numberToString[i]);
-    numberReverseStr += numberToString[i];
+//   //aprch 1
+//   //loop through number from end to begining and add each digit to numberReverse as we go
+//   for (let i = counter; i >= 0; i--) {
+//     // console.log(numberToString[i]);
+//     numberReverseStr += numberToString[i];
+//   }
+//   // console.log(typeof numberReverseStr);
+//   let numberReverseInt = parseInt(numberReverseStr);
+//   return numberReverseInt;
+// };
+
+// console.log(reverseInt(32243));
+
+// 2. Write a JavaScript function that checks whether a passed string is palindrome or not? Go to the editor
+// A palindrome is word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+
+const checkForPalindrom = (strToCheck) => {
+  //need to reverese and store string and then compare 2 values
+  let inversestrToCheck = "";
+  let strToCheckTrimmed = strToCheck.split(" ").join("");
+   console.log('strToCheckTrimmed',strToCheckTrimmed);
+
+  for (let i = strToCheckTrimmed.length - 1; i >= 0; i--) {
+    //console.log(strToCheck[i]);
+    inversestrToCheck += strToCheckTrimmed[i];
   }
-  // console.log(typeof numberReverseStr);
-  let numberReverseInt = parseInt(numberReverseStr);
-  return numberReverseInt;
+
+  // console.log('inversestrToCheck', inversestrToCheck)
+
+  if (strToCheckTrimmed === inversestrToCheck) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-console.log(reverseInt(32243));
+console.log(checkForPalindrom("madamqqqqqqq"));
+console.log(checkForPalindrom("madam"))
+console.log(checkForPalindrom("nurses run"))
+
